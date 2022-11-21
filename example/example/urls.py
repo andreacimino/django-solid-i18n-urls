5cb2757b15a753229754cc2df5159c9b77bcc727
andreacimino <andreacimino@gmail.com>
2022-11-21 07:09:08 +0000

    Fixes

--- example/example/urls.py
+++ example/example/urls.py
@@ -1,5 +1,5 @@
-from django.conf.urls import url, include
 from django.views.generic import TemplateView
+from django.urls import include, re_path as url
 
 from solid_i18n.urls import solid_i18n_patterns
 
